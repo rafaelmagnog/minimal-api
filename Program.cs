6b37fb7f8032609ce4327c54ff54d44bdf62e882
Rafael Magno G. <rafaelmagno.dev@gmail.com>
2025-09-02 23:14:14 -0300

    :arrow_up: feat: atualizando Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,43 @@
 
+using MinimalApi.Infraestrutura.DB;
+using MinimalApi.DTOs;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Servicos;
+using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Dominio.ModelViews;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<IAdministradorService, AdministradorService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => " World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapGet("/", () => Results.Json(new Home()));
+
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorService administradorService) =>
 {
-    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    if (administradorService.Login(loginDTO) != null)
         return Results.Ok("Login com sucesso!");
     else
         return Results.Unauthorized();
 });
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
 
