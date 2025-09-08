adb45cf0857467a61ed343a5cf11711a94d02c42
Rafael Magno G. <rafaelmagno.dev@gmail.com>
2025-09-08 00:54:29 -0300

    :arrow_up: feat(Service): atualizando serviço de administrador

--- Dominio/Servicos/AdministradorService.cs
+++ Dominio/Servicos/AdministradorService.cs
@@ -18,10 +18,36 @@ namespace MinimalApi.Dominio.Servicos
             _contexto = contexto;
         }
 
+        public Administrador Criar(Administrador administrador)
+        {
+            _contexto.Administradores.Add(administrador);
+            _contexto.SaveChanges();
+
+            return administrador;
+        }
+
+        public List<Administrador> Todos(int? pagina)
+        {
+            var query = _contexto.Administradores.AsQueryable();
+
+            int itensPorPagina = 10;
+
+            if (pagina != null)
+            {
+                query = query.Skip(((int)pagina - 1) * itensPorPagina).Take(itensPorPagina);
+            }
+            return query.ToList();
+        }
+
         public Administrador? Login(LoginDTO loginDTO)
         {
             var adm = _contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Senha).FirstOrDefault();
             return adm;
+        }
+
+        public Administrador? BuscaPorId(int id)
+        {
+            return _contexto.Administradores.Where(v => v.Id == id).FirstOrDefault();
         }
     }
 }
