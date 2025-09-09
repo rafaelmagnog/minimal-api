33504730a9c3ef07d36f98d5f81ebdd4c14d590d
Rafael Magno G. <rafaelmagno.dev@gmail.com>
2025-09-09 14:37:08 -0300

    :white_check_mark: :test_tube: test: Criando testes para service de administradores && :card_file_box: raw: cria db para tests

---
+++ Test/Domain/Services/AdministradorService.cs
@@ -0,0 +1,70 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Servicos;
+using MinimalApi.Infraestrutura.DB;
+
+namespace Test.Domain.Entidades;
+
+[TestClass]
+public class AdministradorServiceTest
+{
+    private DbContexto CriarContextoDeTeste()
+    {
+        var assemblyPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        var path = Path.GetFullPath(Path.Combine(assemblyPath ?? "", "..", "..", ".."));
+
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(path ?? Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+
+        return new DbContexto(configuration);
+    }
+
+    [TestMethod]
+    public void TestandoSalvarAdministrador()
+    {
+        // Arrange
+        var context = CriarContextoDeTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
+        var adm = new Administrador();
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "Adm";
+
+        var administradorService = new AdministradorService(context);
+
+        // Act
+        administradorService.Criar(adm);
+
+        // Assert
+        Assert.AreEqual(1, administradorService.Todos(1).Count());
+    }
+
+        [TestMethod]
+    public void TestandoBuscarPorId()
+    {
+        // Arrange
+        var context = CriarContextoDeTeste();
+        context.Database.ExecuteSqlRaw("TRUNCATE TABLE Administradores");
+
+        var adm = new Administrador();
+        adm.Email = "teste@teste.com";
+        adm.Senha = "teste";
+        adm.Perfil = "Adm";
+
+        var administradorService = new AdministradorService(context);
+
+        // Act
+        administradorService.Criar(adm);
+        var admDoBanco = administradorService.BuscaPorId(adm.Id);
+
+        // Assert
+        Assert.AreEqual(1, admDoBanco?.Id);
+    }
+}
